ed0b15be316561830411a20f6cb62a346286a28d
Amran Mesfin <amranmesfin@Amrans-MacBook-Air.local>
2026-01-29 00:34:34 -0500

    Update piano keys styles and index.html

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// Vars
+const WHITE_KEYS = ['z', 'x', 'c', 'v', 'b', 'n', 'm'];
+const BLACK_KEYS = ['s', 'd', 'g', 'h', 'j'];
+
+const whiteKeys = document.querySelectorAll('.key.white');
+const blackKeys = document.querySelectorAll('.key.black');
+
+
+
 // Select all keys
 const keys = document.querySelectorAll('.key');
 
@@ -10,6 +19,42 @@ keys.forEach((key) => {
 
 // Handlers 
 function playNote(key) {
+    // Get the notes letter value
   const noteAudio = document.getElementById(key.dataset.note);
+ 
+  //Reset the notes current time to 0
+  noteAudio.currentTime = 0;
+ 
+ // Play the note
   noteAudio.play();
+
+  // Add a class, active, for styling purposes
+  key.classList.add('active');
+
+  //remove active class
+  noteAudio.addEventListener('ended', () => {
+    key.classList.remove('active');
+  });
+  
 }
+
+
+document.addEventListener('keydown', (e) => {
+  // Get the key that triggered the event
+  const key = e.key;
+  const whiteKeyIndex = WHITE_KEYS.indexOf(key);
+  const blackKeyIndex = BLACK_KEYS.indexOf(key);
+
+  // If the key is being held down, don't play the note again
+  if (e.repeat) {
+    return;
+  }
+  
+  if (whiteKeyIndex > -1) {
+    playNote(whiteKeys[whiteKeyIndex]);
+  }
+
+  if (blackKeyIndex > -1) {
+    playNote(blackKeys[blackKeyIndex]);
+  }
+});
